b9aeb10f3e8bdd1a3f864e5193fb77f876529d8e
senth <senth.wallace@gmail.com>
2022-07-25 15:37:20 +0200

    Fix missing cmd

--- cmd/accman/main.go
+++ cmd/accman/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"github.com/Senth/accman/internal/gateways/verrepo"
 	"github.com/Senth/accman/models"
 	"github.com/spf13/cobra"
 )
@@ -21,7 +20,6 @@ func init() {
 	initParse(rootCmd)
 	initSie(rootCmd)
 	initLedger(rootCmd)
-	rootCmd.AddCommand(fixCmd)
 }
 
 func main() {
@@ -36,34 +34,4 @@ func printVerificationTypes() {
 	fmt.Printf("Direct payment in: %d\n", models.VerificationTypeDirectPayment|models.VerificationTypeIn)
 	fmt.Printf("Direct payment out: %d\n", models.VerificationTypeDirectPayment|models.VerificationTypeOut)
 	fmt.Printf("Transfer: %d\n", models.VerificationTypeTransfer)
-}
-
-var fixCmd = &cobra.Command{
-	Use:   "fix",
-	Short: "Fix the database",
-	Run: func(cmd *cobra.Command, args []string) {
-		repo := verrepo.NewJSONImpl()
-		fys, _ := repo.GetAll()
-		for _, fy := range fys {
-			if fy.From.Year() == "2021" {
-				fy = fix(fy)
-				_ = repo.UpdateFiscalYear(fy)
-			}
-		}
-	},
-}
-
-func fix(fy models.FiscalYear) models.FiscalYear {
-	fy.Commit()
-
-	// Set date to the 10th of the month
-	for i, v := range fy.Verifications {
-		date := v.Date
-		date = date.SetDay("10")
-		date = date.AddMonths(1)
-		v.DateFiled = date
-		fy.Verifications[i] = v
-	}
-
-	return fy
 }
